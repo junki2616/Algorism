2623008edadd00260c9b67b63072510f09cc7c26
junki-nishio <njunki0616@gmail.com>
2020-04-02 01:42:14 +0900

    二分探索法プログラムの作成

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -7,9 +7,32 @@ int main(){
 
     //探索するデータ
     int array[] = {1,3,4,5,8,12,15};
+    //探索したい値
+    int target_num = 12;
 
     int top = 0;
+    int center;
     int bottom = sizeof(array) / sizeof(int) - 1;
 
+    while(1){
+                if(top > bottom){
+                    cout << target_num << "は見つかりませんでした" << endl;
+                    break;
+                }
+                
+                center = (top + bottom) / 2;
+                
+                if(array[center] == target_num){
+                    cout << target_num << "はarray[" << center << "]の中にありました。" << endl;
+                    break;
+                }else{
+                    if(array[center] < target_num){
+                        top = center + 1;                   
+                    }else{
+                        bottom = center - 1;
+                    }
+                }
+            }
+            
     return 0;
 }

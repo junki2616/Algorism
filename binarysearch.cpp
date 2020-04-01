c7074391dc4a43e16c0404037e81cf950d604283
junki-nishio <njunki0616@gmail.com>
2020-04-01 23:55:43 +0900

    no message

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main(){
 
-    unsigned char i;
+    //探索するデータ
+    int array[] = {1,3,4,5,8,12,15};
+
+    int top = 0;
+    int bottom = sizeof(array) / sizeof(int) - 1;
 
     return 0;
 }

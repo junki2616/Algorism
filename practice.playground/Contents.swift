40324b92375f710e401fd4e1118295726b83544f
junki-nishio <njunki0616@gmail.com>
2020-04-22 23:57:39 +0900

    no message

--- practice.playground/Contents.swift
+++ practice.playground/Contents.swift
@@ -1,1 +1,26 @@
 
+protocol hogeProtocol{
+    
+    var setGet:Int {set get}
+    var target:Int {get}
+}
+
+
+class hogeClass:hogeProtocol{
+    
+    var setGet: Int {
+        
+        set(p){
+            target = p - 10
+        }
+        get{
+            return target + 10
+        }
+    }
+    
+    var target:Int = 100
+    
+}
+
+
+
